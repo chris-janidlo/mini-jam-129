33bb94e187ccfada248762f4b2ec4a032c916662
Chris Janidlo <christopherjanidlo@gmail.com>
2023-04-01 18:21:39 -0500

    Snake following movement

--- Assets/Scripts/SnakeHead.cs
+++ Assets/Scripts/SnakeHead.cs
@@ -6,31 +6,57 @@ using UnityEngine.InputSystem;
 
 public class SnakeHead : MonoBehaviour
 {
-    public float MoveSpeed;
+    public float LerpSpeed;
+    public int MaxFollowers;
     public Vector2Int InitialMoveDirection;
 
     public new Rigidbody2D rigidbody;
 
-    Vector2Int latestInput, lockedInput, destination;
+    float lerp;
+    Vector2Int latestInput, currentMoveDirection;
 
+    LinkedList<Vector2> destinationBuffer;
+    List<SnakeMonster> followers;
+    
     void Start()
     {
-        latestInput = lockedInput = InitialMoveDirection;
+        latestInput = currentMoveDirection = InitialMoveDirection;
 
-        destination = Vector2Int.RoundToInt(rigidbody.position);
-        rigidbody.MovePosition(destination); // ensure we start on the grid
+        destinationBuffer = new LinkedList<Vector2>();
+        followers = new List<SnakeMonster>(MaxFollowers);
+
+        var start = Vector2Int.RoundToInt(rigidbody.position);
+        rigidbody.MovePosition(start); // ensure we start on the grid
+
+        destinationBuffer.AddFirst(start);
+        destinationBuffer.AddFirst(start + currentMoveDirection);
     }
 
     void FixedUpdate()
     {
-        if (rigidbody.position == destination)
-        {
-            lockedInput = latestInput;
-            destination += lockedInput;
-        }
+        moveSnake();
 
-        var newPos = Vector2.MoveTowards(rigidbody.position, destination, MoveSpeed * Time.deltaTime);
-        rigidbody.MovePosition(newPos);
+        lerp += LerpSpeed * Time.deltaTime;
+
+        if (lerp >= 1)
+        {
+            lerp = 0;
+            currentMoveDirection = latestInput;
+            updateDestination();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (
+            other.GetComponent<SnakeMonster>() is { } follower
+            && !follower.Following
+            && followers.Count < MaxFollowers
+        )
+        {
+            followers.Insert(0, follower);
+            follower.Following = true;
+        }
     }
 
     void OnMove(InputValue value)
@@ -38,11 +64,41 @@ public class SnakeHead : MonoBehaviour
         var input = value.Get<Vector2>();
         if (input == Vector2.zero) return;
 
-        var candidateInput = Mathf.Abs(input.x) > Mathf.Abs(input.y)
+        var cardinalizedInput = Mathf.Abs(input.x) > Mathf.Abs(input.y)
             ? new Vector2Int(Math.Sign(input.x), 0)
             : new Vector2Int(0, Math.Sign(input.y));
 
         // can't turn 180 degrees
-        if (candidateInput != -lockedInput) latestInput = candidateInput;
+        if (cardinalizedInput != -currentMoveDirection) latestInput = cardinalizedInput;
+    }
+
+    void moveSnake()
+    {
+        var node = destinationBuffer.First;
+        for (var i = -1; i < followers.Count; i++)
+        {
+            var newPos = Vector2.Lerp(node.Next.Value, node.Value, lerp);
+
+            var rbToMove = i == -1
+                ? rigidbody
+                : followers[i].rigidbody;
+
+            rbToMove.MovePosition(newPos);
+
+            node = node.Next;
+        }
+    }
+
+    void updateDestination()
+    {
+        var nextDestination = destinationBuffer.First.Value + currentMoveDirection;
+        destinationBuffer.AddFirst(nextDestination);
+
+        // need two extra destinations because each segment (including the head) is
+        // lerping between 2 destinations
+        if (destinationBuffer.Count > MaxFollowers + 2)
+        {
+            destinationBuffer.RemoveLast();
+        }
     }
 }
---
+++ Assets/Scripts/SnakeMonster.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMonster : MonoBehaviour
+{
+    public bool Following;
+    public new Rigidbody2D rigidbody;
+}
